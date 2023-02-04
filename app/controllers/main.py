bf071fcb47deacb0fa95eefedfe33c7c13974307
Marissa Posner <46746064+marissaposner@users.noreply.github.com>
2023-02-04 09:47:15 -0500

    Linkroot (#18)
    * connecting backend data to the controller and front end
    
    * 🔥 remove result json file from versioning
    
    ---------
    
    Co-authored-by: bal7hazar <bal7hazar@proton.me>

--- app/controllers/main.py
+++ app/controllers/main.py
@@ -1,9 +1,11 @@
 # import asyncio
 
 from app.models.base import BaseModel
-from app.models.briq import DATA_PATH, DATA_PATH_L2
-# from app.models.starknetid import DATA_PATH, DATA_PATH_L2
+from app.models.sybildetection1.base import SybilModel
+# from app.models.briq import DATA_PATH, DATA_PATH_L2
+
 # from app.models.indexer import Indexer
+from app.models.sybildetection1 import DATA_PATH
 
 import json
 
@@ -15,7 +17,7 @@ class MainController:
         # indexer = Indexer()
         # asyncio.run(indexer.run())
 
-        model = BaseModel.from_json(DATA_PATH, DATA_PATH_L2)
+        model = SybilModel.from_json(DATA_PATH)
         result = model.run(address)
         with open('result.json', 'w') as fout:
             json.dump(result, fout, indent=2)
